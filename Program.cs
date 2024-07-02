fa2b795b515bead9eb99eb6e8b40e18ca98e8c94
gcanalonga <canalongag@gmail.com>
2024-07-02 15:25:50 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("13 99999-9999", "G60 5G", "111111111111111", 64);
+        Smartphone iphone = new Iphone("11 97777-7777", "15 Pro", "222222222222222", 128);
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
